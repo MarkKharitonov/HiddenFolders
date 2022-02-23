cf5348bc7a8a657a165370cce5c7ac115e497f85
Kharitonov, Mark <mark.kharitonov@ceridian.com>
2022-02-23 12:35:58 -0500

    Initial commit

---
+++ LegacyFrontEnd/Approvals/Scripts/Approvals/package.profile.js
@@ -0,0 +1,33 @@
+﻿var profile = (function() {
+
+    var exclude = function(filename, mid) {
+        return /.*\/bin\/.*/.test(filename) || /.*\/Approvals\/(Tests)\/.*/.test(filename) || !/Approvals\/.*/.test(mid) || /.*\.map.js$/.test(filename) || /.*\.ts$/.test(filename);
+    };
+
+    var copyOnly = function(filename, mid) {
+        return !/.*\.js$/.test(filename);
+    }
+
+    var amd = function(filename, mid) {
+        return !exclude(filename, mid);
+    };
+
+    var frameworkProfile = 
+    {
+        resourceTags: {
+            amd: function(filename, mid) {
+                return amd(filename, mid);
+            },
+            miniExclude: function(filename, mid)
+            {
+                return exclude(filename, mid);             
+            },
+            copyOnly:function(filename, mid)
+            {
+                return copyOnly(filename, mid);
+            }
+        }
+    };
+
+    return frameworkProfile;
+})();
